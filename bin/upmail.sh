09752f22def86dc70c3b8e3e3cbfcd456a933d9a
13 <13@users.noreply.github.com>
2021-05-05 11:05:06 +0200

    shellcheck

--- bin/upmail.sh
+++ bin/upmail.sh
@@ -7,11 +7,11 @@ LOCKFILE="$HOME/.msmtpqueue/.lock"
 
 TIME="$(date +"%H:%M:%S")"
 
-if [ -f $LOCKFILE ]; then
-  rm $LOCKFILE
+if [ -f "$LOCKFILE" ]; then
+  rm "$LOCKFILE"
 fi
-[ ! -z "$1" ] && MAIL="$1"
-echo -e "Subject: $(hostname) ON $TIME\n\n" | $MAILER $MAIL &
+[ -n "$1" ] && MAIL="$1"
+echo -e "Subject: $(hostname) ON $TIME\n\n" | $MAILER "$MAIL" &
 sleep 1
-$HOME/bin/msmtp-runqueue.sh &
+"$HOME"/bin/msmtp-runqueue.sh &
 exit 0
